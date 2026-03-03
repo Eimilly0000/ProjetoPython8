d1056ff25365a681e98a2605234e7d91fdd1f244
Eimilly__S2 <emillysantosaaraujo@gmail.com>
2026-03-03 18:35:36 -0300

    Add files via upload
    8.                  Receba o valor de um depósito em poupança. Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.

---
+++ Questão8.py
@@ -0,0 +1,15 @@
+#Declarar
+
+Valor : int = 0
+Rendimento : int = 0
+Total : int = 0
+
+#Inicio
+
+Valor = int (input("Digite o Valor do Depósito: "))
+Rendimento = (Valor * 0.013)
+Total = (Valor + Rendimento)
+
+print ("O valor do Depósito rendido após um mês é ", Total)
+
+#Fim
